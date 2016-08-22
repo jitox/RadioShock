e12e9afa423c30fca00dbde59bee7484a321401a
Jitox <alejandroaguiar@hotmail.com>
2016-08-22 17:08:26 -0300

    Added first iteration of parser

---
+++ Assets/Script/game/CMapParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+public static class CMapParser
+{
+
+
+    public static int[][] readMap(string aFile)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(aFile);
+        List<string> cosoNodes = new List<string>();
+        
+        foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+        {
+            string text = node.InnerText; //or loop through its children as well
+            cosoNodes.Add(text);
+        }
+        string map = cosoNodes[1];
+        string[] tokens = map.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        List<List<int>> intTileMap = new List<List<int>>();
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            List<int> lineinInt = new List<int>();
+            string[] line = tokens[i].Split(',');
+            for (int j = 0; j < line.Length-1; j++)
+            {
+                lineinInt.Add(int.Parse(line[j]));
+                
+            }
+            intTileMap.Add(lineinInt);
+           
+        }
+      return intTileMap.Select(a => a.ToArray()).ToArray();
+    }
+}
+
